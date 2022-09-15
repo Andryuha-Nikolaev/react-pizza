c76ed29b8604c0be7af5915e4cb42162b014867f
admin <andryuha_nikolaev@mail.ru>
2022-09-15 17:30:49 +0300

    refactor: создать React компоненты

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import './scss/app.scss';
+
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Карбонара" price={450} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
