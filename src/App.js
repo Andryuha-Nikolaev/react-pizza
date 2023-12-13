43c6a23178e16541352301334f31eab90a2a3518
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2023-12-13 18:18:07 +0300

    #19.1 react-router-dom Outlet

--- src/App.js
+++ src/App.js
@@ -3,25 +3,23 @@ import { Routes, Route } from 'react-router-dom';
 
 import './scss/app.scss';
 
-import Header from './components/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 import FullPizza from './pages/FullPizza';
 
+import MainLayout from './layouts/MainLayout';
+
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/cart" element={<Cart />} />
-          <Route exact path="/pizza/:id" element={<FullPizza />} />
-          <Route exact path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<MainLayout />}>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/pizza/:id" element={<FullPizza />} />
+        <Route exact path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
